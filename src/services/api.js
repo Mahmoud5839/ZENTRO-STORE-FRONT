2529ad570021b16e1f2aa26f3b7fb82395e579f5
Mahmoud5839 <hooda7113@gmail.com>
2026-05-05 00:31:27 +0300

    api commit

--- src/services/api.js
+++ src/services/api.js
@@ -1,6 +1,6 @@
 import axios from 'axios';
 
-const API_URL = 'http://localhost:5000/api';
+const API_URL = 'http://zentro-store-5000.vercel.app/api';
 
 const api = axios.create({
     baseURL: API_URL,
